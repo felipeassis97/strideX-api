fa5906a73600a853677c8903cf3acba15a21ffcb
felipeassis97 <felipeassis97@gmail.com>
2024-08-03 10:09:04 +1000

    chore: update comments

--- src/main/resources/db/migration/V2__mock_data.sql
+++ src/main/resources/db/migration/V2__mock_data.sql
@@ -1,35 +1,35 @@
--- Inserindo um endereço
+-- Inserting an address
 INSERT INTO address (street, neighborhood, city, state, country, zipcode, lat, lng)
 VALUES ('123 Main St', 'Downtown', 'Springfield', 'IL', 'USA', '62701', 39.7817, -89.6501);
 
--- Inserindo uma loja e associando ao endereço criado
+-- Inserting a store and associating it with the created address
 INSERT INTO store (name, description, logo, address_id)
 VALUES ('My Store', 'A great store', 'logo.png', LAST_INSERT_ID());
 
--- Inserindo uma marca
+-- Inserting a brand
 INSERT INTO brand (name)
 VALUES ('Brand A');
 
--- Inserindo um gênero
+-- Inserting a genre
 INSERT INTO genre (name)
 VALUES ('Casual');
 
--- Inserindo um produto e associando à loja e à marca criadas
+-- Inserting a product and associating it with the created store and brand
 INSERT INTO product (store_id, brand_id, title, description, price, quantity)
 VALUES (LAST_INSERT_ID(), LAST_INSERT_ID(), 'Product 1', 'Description of product 1', 29.99, 100);
 
--- Inserindo uma imagem do produto e associando ao produto criado
+-- Inserting a product image and associating it with the created product
 INSERT INTO product_image (product_id, url)
 VALUES (LAST_INSERT_ID(), 'http://example.com/product1.png');
 
--- Inserindo um tamanho
+-- Inserting a size
 INSERT INTO size (size)
 VALUES ('M');
 
--- Associando o produto ao tamanho
+-- Associating the product with the size
 INSERT INTO product_size (product_id, size_id)
 VALUES (LAST_INSERT_ID(), LAST_INSERT_ID());
 
--- Associando o produto ao gênero
+-- Associating the product with the genre
 INSERT INTO product_genre (product_id, genre_id)
 VALUES (LAST_INSERT_ID(), LAST_INSERT_ID());
