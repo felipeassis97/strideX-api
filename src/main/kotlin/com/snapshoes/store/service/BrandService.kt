308930a792870c32c1aecfe67e45e6669de0b3fe
felipeassis97 <felipeassis97@gmail.com>
2024-08-19 08:30:06 +1000

    feat: delete brand by id

--- src/main/kotlin/com/snapshoes/store/service/BrandService.kt
+++ src/main/kotlin/com/snapshoes/store/service/BrandService.kt
@@ -5,12 +5,14 @@ import org.springframework.cache.annotation.Cacheable
 import com.snapshoes.store.config.exceptions.NotFoundException
 import com.snapshoes.store.presentation.dtos.mappers.BrandMapper
 import com.snapshoes.store.persistense.repositories.BrandRepository
+import com.snapshoes.store.persistense.repositories.ProductRepository
 import com.snapshoes.store.presentation.dtos.response.common.BrandDto
 
 @Service
 class BrandService(
     private val brandMapper: BrandMapper,
-    private val brandRepository: BrandRepository
+    private val brandRepository: BrandRepository,
+    private val productRepository: ProductRepository,
 ) {
     @Cacheable(cacheNames = ["Brands"], key = "#root.method.name")
     fun getBrands(): List<BrandDto> {
@@ -23,5 +25,17 @@ class BrandService(
         val brand = brandRepository.findById(id)
             .orElseThrow { NotFoundException("Brand NOT FOUND") }
         return brandMapper.toDto(brand)
+    }
+
+
+    fun deleteBrandById(id: Long) {
+        // Fetch all products by brandId
+        val products = productRepository.findByBrandId(id)
+
+        // Delete everyone
+        if (products.isNotEmpty()) {
+            productRepository.deleteAll(products)
+        }
+        brandRepository.deleteById(id)
     }
 }
